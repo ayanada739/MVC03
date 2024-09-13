41be25f39a4beb9d443348e6c0d69e2ec302f0a9
ayanada739 <ayanada739@gmail.com>
2024-09-14 01:12:40 +0300

    Add project files.

---
+++ Company.G03.DAL/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Company.G03.DAL.Data.Configurations;
+using Company.G03.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G03.DAL.Data.Contexts
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = CompanyMVCG03; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}

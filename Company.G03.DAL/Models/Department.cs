a48b610c44a5b4d1c4c24d9b747f3897462d2090
ayanada739 <ayanada739@gmail.com>
2024-09-14 05:07:08 +0300

    Department Controller - Index

--- Company.G03.DAL/Models/Department.cs
+++ Company.G03.DAL/Models/Department.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,17 @@ namespace Company.G03.DAL.Models
 {
     public class Department
     {
+        
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Code Is Required!")]
         public string Code { get; set; }
+
+        [Required (ErrorMessage ="Name Is Required!")]
+
         public string Name { get; set; }
+
+        [DisplayName("Date Of Creation")]
         public DateTime DateOfCreation { get; set; }
     }
 }
